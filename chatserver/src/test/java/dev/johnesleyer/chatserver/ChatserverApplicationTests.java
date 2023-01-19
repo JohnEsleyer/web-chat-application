e8d808bbeb8507dd101cff0914a56d12b288d76c
john <ralphesleyer@gmail.com>
2023-01-19 22:12:23 +0800

    added and initialized chatserver springboot app

---
+++ chatserver/src/test/java/dev/johnesleyer/chatserver/ChatserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johnesleyer.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
